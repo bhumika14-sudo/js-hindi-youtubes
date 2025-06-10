407cc7fa3b68bdb57c78f3c156b4d13df1bdbbe4
bhumika14-sudo <bhumi.kagoyal1402@gmail.com>
2025-06-10 15:36:54 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = "1234"
+let accountPassword = "232"
+let accountEmail = "bhumika@google.com"
+let accountCity = "jaipur"
+
+
+accountPassword = "333"               
+accountEmail = "bhumika12@google.com" 
+accountCity = "delhi"   
+
+console.table([accountId,accountPassword,accountEmail,accountCity])
+
+
+
---
+++ 01_basics/02-datatypes.js
@@ -0,0 +1,20 @@
+"use strict";
+let name ="bhumika"
+let age="18"
+let state = "null"
+
+
+//treat all js code in newer version
+//number -> 2 to power 53
+// bigint
+// string = " "
+// boolean = true/false
+// null = standalone value
+// undefined
+// symbol = unique
+
+console.log(typeof null)
+
+console.log(typeof name)
+
+console.log(typeof age)
