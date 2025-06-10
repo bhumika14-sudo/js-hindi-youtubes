be82ea6422cf16d16515364bbe02bb1969a3aec2
bhumika14-sudo <bhumi.kagoyal1402@gmail.com>
2025-06-10 16:13:28 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score = "33"
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+// "33" -> 33
+// "33abd"-> NaN
+// true -> 1 , false -> 0
+// "bhumika" -> NaN
+
+// let isLoggedIn = true;
+// let valueinBoolean = Boolean(isLoggedIn)
+// console.log(typeof isLoggedIn)
+
+let isLoggedIn = 1;
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanisLoggedIn )
