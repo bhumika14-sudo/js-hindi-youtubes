af6dde96b7516c26f2fecc239266f393423166db
bhumika14-sudo <bhumi.kagoyal1402@gmail.com>
2025-06-11 10:43:54 +0000

    datatype-summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,48 @@
+//primitve datatypess
+
+// 7 types : string , number , boolean , null , undefined ,
+//  symbol , BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLogeedIn = false
+const outsideTemp = null
+let userEmail;
+// const id = symbol('123')
+// const anotherId = symbol("123")
+
+// console.log(id===anotherId);
+
+const bigNumber = 53781621956107682463879n
+
+console.log(score)
+console.log(scoreValue)
+console.log(isLoggedIn)
+console.log(outsideTemp)
+console.log(userEmail)
+// console.log(id)
+// console.log(anotherId)
+
+// Reference (Non Primitive)
+
+// Array, Object, Functions
+
+// Array
+const heors = ["shaktiman","naagraj"," doga"]
+
+// Object
+let myObj = {
+    name : "bhumika",
+    age : 18
+}
+
+// Functions
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction)
+
+
+
+
