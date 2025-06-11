9c0034c90dc3f22017c308420c12847402f55201
bhumika14-sudo <bhumi.kagoyal1402@gmail.com>
2025-06-11 14:12:24 +0000

    nums and maths

---
+++ 01_basics/05_nums_and_maths.js
@@ -0,0 +1,37 @@
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 23.567
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds = 10000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+//+++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math)
+console.log(Math.abs(-4))  // neg=pos, pos!=neg
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.6))
+console.log(Math.floor(4.6))
+console.log(Math.min(3,4,5,6))
+console.log(Math.max(3,4,5,6))
+
+console.log(Math.random()) //value0-1
+console.log(Math.floor(Math.random()*10) + 1) // kha se kha tak values chahiye 
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
